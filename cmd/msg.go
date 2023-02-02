8fb0eb64891458c6a70ad507fe2452663ec3d587
chramb <79099832+chramb@users.noreply.github.com>
2023-02-02 10:35:33 +0100

    cli help for msg

--- cmd/msg.go
+++ cmd/msg.go
@@ -7,10 +7,12 @@ import (
 )
 
 var msgCmd = &cli.Command{
-	Name: "message",
+	Name:    "message",
+	Usage:   "test or just execute message action",
+	Aliases: []string{"m"},
 	Flags: []cli.Flag{
-		&cli.StringFlag{Name: "Discord", Aliases: []string{"d"}, Action: discord.Message},
-		&cli.StringFlag{Name: "Mail", Aliases: []string{"m"}, Action: mail.Mail},
+		&cli.StringFlag{Name: "Discord", Aliases: []string{"d"}, Action: discord.Message, Usage: "run discord action from provided config [discord.`VALUE`]"},
+		&cli.StringFlag{Name: "Mail", Aliases: []string{"m"}, Action: mail.Mail, Usage: "run mail action from provided config [mail.`VALUE`]"},
 	},
 	Action: cli.ShowSubcommandHelp,
 }
